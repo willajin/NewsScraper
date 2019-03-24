94f87653a3114a7b0ee1d75ef12655a6c218215c
Willa Jin <jin.willa@gmail.com>
2019-03-23 20:22:57 -0700

    fixed scraping

--- server.js
+++ server.js
@@ -21,31 +21,45 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// set up handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // connect to MongoDB
 mongoose.connect("mongodb://localhost/Week18Homework", { useNewUrlParser: true });
 
 // routes
 // GET route to scrape news website
-app.get("/scrape", function(req, res) {
+app.get("/scrape", function (req, res) {
     // axios
-    axios.get("").then(function(response) {
+    axios.get("https://www.vox.com/").then(function (response) {
         // load to cheerio
         var $ = cheerio.load(response.data);
 
         // grab tag in article to save properties
-        $("").each(function(i, element) {
-            var result = {};
+        $(".c-entry-box--compact__title").each(function (i, element) {
+            const result = {};
 
             // save title & link of article
+            result.headline = $(this)
+                .children("a")
+                .text();
+            if ($(this).parent().children('p').text()) {
+                result.summary = $(this).parent().children('p').text()
+            }
+            result.url = $(this)
+                .children("a")
+                .attr("href");
 
-            // create new Article
+            //create new Article
             db.Article.create(result)
-            .then(function(dbArticle) {
-                console.log(dbArticle);
-            })
-            .catch(function(err) {
-                console.log(err);
-            });
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
         });
 
         res.send("Scrape complete.");
@@ -53,21 +67,43 @@ app.get("/scrape", function(req, res) {
 });
 
 // GET route to get all articles from db
-app.get("/articles", function(req, res) {
-
+app.get("/articles", function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
 // GET route to get specific article and populate with corresponding note
-app.get("/articles/:id", function(req, res) {
-
+app.get("/articles/:id", function (req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
 // POST route to update & save note to corresponding article
-app.post("/articles/:id", function(req, res) {
-
+app.post("/articles/:id", function (req, res) {
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
 // start server listen
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App running on port " + PORT);
-  });
+});
